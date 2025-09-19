c1766e0cb9f8d7253cf0520010387740610c351a
Haruki Nagayama <haruki.nagayama03110311hn@gmail.com>
2025-09-19 17:42:51 +0900

    サイトの７完了

--- Assets/Scripts/Actor/ActorSprite.cs
+++ Assets/Scripts/Actor/ActorSprite.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 /// <summary>
 /// アクターのスプライトを設定するクラス
@@ -12,10 +13,13 @@ public class ActorSprite : MonoBehaviour
 
     // 画像素材参照
     public List<Sprite> walkAnimationRes; // 歩行アニメーション(装備別*コマ数)
+    public List<Sprite> stuckSpriteRes; // スタンスプライト(装備別)
 
     // 各種変数
     private float walkAnimationTime; // 歩行アニメーション経過時間
     private int walkAnimationFrame; // 歩行アニメーションの現在のコマ番号
+    private Tween blinkTween;   // 点滅処理Tween
+    public bool stuckMode;      // スタン画像表示モード
 
     // 定数定義
     private const int WalkAnimationNum = 3; // 歩行アニメーションの1種類あたりの枚数
@@ -32,6 +36,17 @@ public class ActorSprite : MonoBehaviour
     // Update
     void Update()
     {
+        // 被撃破中なら終了
+        if (actorController.isDefeat)
+            return;
+
+        // スタン画像表示モード中ならスタン画像を表示
+        if (stuckMode)
+        {
+            spriteRenderer.sprite = stuckSpriteRes[0];
+            return;
+        }
+
         // 歩行アニメーション時間を経過(横移動している間のみ)
         if (Mathf.Abs(actorController.xSpeed) > 0.0f)
             walkAnimationTime += Time.deltaTime;
@@ -49,5 +64,43 @@ public class ActorSprite : MonoBehaviour
         // 歩行アニメーション更新
         spriteRenderer.sprite =
             walkAnimationRes[walkAnimationFrame];
+    }
+
+    /// <summary>
+    /// 点滅開始処理
+    /// </summary>
+    public void StartBlinking()
+    {
+        // DoTweenを使った点滅処理
+        blinkTween = spriteRenderer.DOFade(0.0f, 0.15f) // 1回分の再生時間:0.15秒
+            .SetDelay(0.3f) // 0.3秒遅延
+            .SetEase(Ease.Linear) // 線形変化
+            .SetLoops(-1, LoopType.Yoyo);    // 無限ループ再生(偶数回は逆再生)
+    }
+    /// <summary>
+    /// 被撃破演出開始
+    /// </summary>
+    public void StartDefeatAnim()
+    {
+        // 被撃破スプライト表示
+        spriteRenderer.sprite = stuckSpriteRes[0];
+        // 点滅演出終了
+        if (blinkTween != null)
+            blinkTween.Kill();
+        // スプライト非表示化アニメーション(DOTween)
+        spriteRenderer.DOFade(0.0f, 2.0f); // 2.0秒かけてスプライトの非透明度を0.0fにする
+    }
+    /// <summary>
+    /// 点滅終了処理
+    /// </summary>
+    public void EndBlinking()
+    {
+        // DoTweenの点滅処理を終了させる
+        if (blinkTween != null)
+        {
+            blinkTween.Kill(); // Tweenを終了
+            spriteRenderer.color = Color.white; // 色を元に戻す
+            blinkTween = null;  // Tween情報を初期化
+        }
     }
 }
