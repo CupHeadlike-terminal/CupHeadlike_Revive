d1cde6fbe983a7d41420631a2212cb929ff5f5f2
Haruki Nagayama <haruki.nagayama03110311hn@gmail.com>
2025-10-01 09:26:11 +0900

    Enemy_Wolf実装、ステージ8,9割完成

--- Assets/Scripts/Actor/ActorController.cs
+++ Assets/Scripts/Actor/ActorController.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -28,12 +29,15 @@ public class ActorController : MonoBehaviour
     private float remainStuckTime; // 残り硬直時間(0以上だと行動できない)
     private float invincibleTime;   // 残り無敵時間(秒)
     [HideInInspector] public bool isDefeat; // true:撃破された(ゲームオーバー)
+    [HideInInspector] public bool inWaterMode; // true:水中モード(メソッドから変更)
 
     // 定数定義
-    private const int InitialHP = 20;           // 初期HP(最大HP)
+    private const int InitialHP = 30;           // 初期HP(最大HP)
     private const float InvicibleTime = 2.0f;   // 被ダメージ直後の無敵時間(秒)
     private const float StuckTime = 0.5f;       // 被ダメージ直後の硬直時間(秒)
     private const float KnockBack_X = 2.5f;     // 被ダメージ時ノックバック力(x方向)
+    private const float WaterModeDecelerate_X = 0.8f;// 水中でのX方向速度倍率
+    private const float WaterModeDecelerate_Y = 0.92f;// 水中でのY方向速度倍率
 
     // Start（オブジェクト有効化時に1度実行）
     void Start()
@@ -52,7 +56,7 @@ public class ActorController : MonoBehaviour
 
         // 変数初期化
         rightFacing = true; // 最初は右向き
-        nowHP = maxHP = InitialHP; // 初期HP
+        nowHP = maxHP = InitialHP;
     }
 
     // Update（1フレームごとに1度ずつ実行）
@@ -160,8 +164,8 @@ public class ActorController : MonoBehaviour
         // ジャンプ操作
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {// ジャンプ開始
-         // 接地していないなら終了
-            if (!groundSensor.isGround)
+         // 接地していないなら終了(水中であれば続行)
+            if (!groundSensor.isGround && !inWaterMode)
                 return;
 
             // ジャンプ力を計算
@@ -200,8 +204,34 @@ public class ActorController : MonoBehaviour
         // X方向の速度を入力から決定
         velocity.x = xSpeed;
 
+        // 水中モードでの速度
+        if (inWaterMode)
+        {
+            velocity.x *= WaterModeDecelerate_X;
+            velocity.y *= WaterModeDecelerate_Y;
+        }
+
         // 計算した移動速度ベクトルをRigidbody2Dに反映
         rigidbody2D.linearVelocity = velocity;
+    }
+
+    /// <summary>
+    /// 水中モードをセットする
+    /// </summary>
+    /// <param name="mode">true:水中にいる</param>
+    public void SetWaterMode(bool mode)
+    {
+        // 水中モード
+        inWaterMode = mode;
+        // 水中での重力
+        if (inWaterMode)
+        {
+            rigidbody2D.gravityScale = 0.3f;
+        }
+        else
+        {
+            rigidbody2D.gravityScale = 1.0f;
+        }
     }
     #endregion
 
