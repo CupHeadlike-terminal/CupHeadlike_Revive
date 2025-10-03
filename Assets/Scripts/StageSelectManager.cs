76633668a5d5aea6115c5571c3148c54db91116e
Haruki Nagayama <haruki.nagayama03110311hn@gmail.com>
2025-10-03 19:18:50 +0900

    ステージセレクト画面、復活ボタン

---
+++ Assets/Scripts/StageSelectManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// ステージセレクト画面管理クラス
+/// </summary>
+public class StageSelectManager : MonoBehaviour
+{
+    /// <summary>
+    /// ステージ選択ボタン押下時処理
+    /// </summary>
+    /// <param name="bossID">該当ボスID</param>
+    public void OnStageSelectButtonPressed(int bossID)
+    {
+        // シーン切り替え
+        SceneManager.LoadScene(bossID + 1);
+    }
+}
